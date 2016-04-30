428ffbeda95e9d1cd2c8012889a9220836c09f14
Hid <mikeygulati@gmail.com>
2016-04-30 09:33:02 -0400

    brace spacing edit

--- src/net/ihid/smarttournament/util/ChatUtil.java
+++ src/net/ihid/smarttournament/util/ChatUtil.java
@@ -7,7 +7,7 @@ import org.bukkit.ChatColor;
  */
 public class ChatUtil {
 
-    public static String color(String s){
+    public static String color(String s) {
         return ChatColor.translateAlternateColorCodes('&', s);
     }
 }
