547afbf580d963937d58607d811409497d959498
devhid <devhid@users.noreply.github.com>
2016-05-12 08:52:48 -0400

    Update TournamentListener.java

--- src/net/ihid/smarttournament/listeners/TournamentListener.java
+++ src/net/ihid/smarttournament/listeners/TournamentListener.java
@@ -62,8 +62,13 @@ public class TournamentListener implements Listener {
                 break;
 
             case ACTIVE:
-                api.getPlayers().remove(ps);
-                api.getWinners().remove(ps);
+                if(api.getPlayers().contains(ps)) {
+                    api.getPlayers().remove(ps);
+                }
+                
+                if(api.getPlayers().contains(ps)) {
+                    api.getWinners().remove(ps);
+                }
 
                 for(Match match : api.getMatches()) {
                     if(match.toSet().contains(ps)) {
@@ -72,11 +77,14 @@ public class TournamentListener implements Listener {
                         } else {
                             match.setWinner(match.getFirstPlayer());
                         }
-
+                        
                         deadPlayers.add(ps);
-                        api.addWinner(match.getWinner());
+                        
+                        match.getArena().setOccupied(false);
+                        match.setArena(null);
                         match.getWinner().teleport(api.getSpectatorArea());
 
+                        api.addWinner(match.getWinner());
                         api.endMatch(match);
                         break;
                     }
@@ -107,8 +115,13 @@ public class TournamentListener implements Listener {
                 break;
 
             case ACTIVE:
-                api.getPlayers().remove(ps);
-                api.getWinners().remove(ps);
+                if(api.getPlayers().contains(ps)) {
+                    api.getPlayers().remove(ps);
+                }
+                
+                if(api.getPlayers().contains(ps)) {
+                    api.getWinners().remove(ps);
+                }
 
                 for(Match match : api.getMatches()) {
                     if(match.toSet().contains(ps)) {
@@ -117,6 +130,9 @@ public class TournamentListener implements Listener {
                         } else {
                             match.setWinner(match.getFirstPlayer());
                         }
+                        
+                        match.getArena().setOccupied(false);
+                        match.setArena(null);
                         match.toSet().forEach(player -> player.teleport(api.getSpectatorArea()));
 
                         api.addWinner(match.getWinner());
@@ -125,8 +141,6 @@ public class TournamentListener implements Listener {
                     }
                     break;
                 }
-                break;
-            case FINISHED:
                 break;
             default:
                 break;
